6ccb6ac3501d7b91570b98e8ddb58f63d9b29888
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-08-10 15:33:43 -0300

    feat: Headder structure

---
+++ src/Components/Headder/Headder.component.tsx
@@ -0,0 +1,16 @@
+
+export function Headder(){
+
+    return(
+        <>
+        <div>
+            <p>Lord Dain</p>
+            <p>(62) 98762-8293</p>
+        </div>
+        <div>
+            <button>Criar contato</button>
+            <button>Logout</button>
+        </div>
+        </>
+    )
+};
--- src/Pages/UserDashBoard/UserDashBoard.tsx
+++ src/Pages/UserDashBoard/UserDashBoard.tsx
@@ -1,8 +1,10 @@
+import { Headder } from "../../Components/Headder/Headder.component";
 
 export function UserDashBoard(){
 
     return(
         <>
+            <Headder/>
             <p>Página do usuário</p>
         </>
     )
